3c98f33aa38287afbacd773f342a0fda0486f21b
Manan Arora <mananarora537@gmail.com>
2025-03-11 12:22:20 +0530

    Update PropertyFeatures.tsx

--- src/components/PropertyFeatures.tsx
+++ src/components/PropertyFeatures.tsx
@@ -23,8 +23,8 @@ const PropertyFeatures = () => {
       possession: 'Ready to move',
       offers: ['Free maintenance for 1 year', '5% discount on early booking'],
       images: [
-        'media/IMG-20250309-WA0063.jpg',
-        'media/IMG-20250309-WA0066.jpg',
+        { src: '/media/IMG-20250309-WA0063.jpg' },
+        { src: '/media/IMG-20250309-WA0066.jpg' },
       ]
     },
     '2bhk': {
@@ -35,8 +35,8 @@ const PropertyFeatures = () => {
       possession: 'Ready to move',
       offers: ['Free club membership for 2 years', 'Kitchen appliances included'],
       images: [
-        'media/IMG-20250309-WA0070.jpg',
-        'media/IMG-20250309-WA0067.jpg',
+        { src: '/media/IMG-20250309-WA0070.jpg' },
+        { src: '/media/IMG-20250309-WA0067.jpg' },
       ]
     },
     '3bhk': {
@@ -47,8 +47,8 @@ const PropertyFeatures = () => {
       possession: 'Ready to move',
       offers: ['Home automation system included', 'Complimentary interior design consultation'],
       images: [
-        'media/IMG-20250309-WA0069.jpg',
-        'media/IMG-20250309-WA0068.jpg',
+        { src: '/media/IMG-20250309-WA0069.jpg' },
+        { src: '/media/IMG-20250309-WA0068.jpg' },
       ]
     },
   };
@@ -97,7 +97,7 @@ const PropertyFeatures = () => {
                 <CarouselItem key={index}>
                   <div className="relative rounded-xl overflow-hidden aspect-[4/3]">
                     <img 
-                      src={image} 
+                      src={image.src} 
                       alt={`${activeProperty.title} - View ${index + 1}`} 
                       className="w-full h-full object-contain bg-gray-100"
                     />
